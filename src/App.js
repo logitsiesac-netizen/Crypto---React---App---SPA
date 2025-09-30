00c696c04478a6338afec9ae51bc6c7214267336
LogIT <diegojaimessantos@gmail.com>
2025-09-30 14:46:09 -0500

    feat: proyecto inicial React con consumo de API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    const [cryptos, setCryptos] = useState([]);
+    const [search, setSearch] = useState(""); 
+    useEffect(() => { 
+    fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+      .then((res) => res.json())
+      .then((data) => setCryptos(data)) 
+      .catch((error) => console.error("error al traer los datos ", error));
+     }, []);
+
+     const filteredCryptos = crypstos.filter((coin) =>
+      coin.name.toLowerCase().includes(search.toLowerCase())
+  
+    );
+
+    return (
+      <div>
+        <h1>Lista de cryptomonedas $ </h1>
+        <ul>
+          {cryptos.map((coin) => (
+            <li key = {coin.id}>
+             {coin.name} - ${coin.current_price}            
+             </li>
+          ))}
+    </ul>
+   </div>
+  );           
 }
 
 export default App;
+   
+//    <div className="App">
+ //     <header className="App-header">
+ //       <img src={logo} className="App-logo" alt="logo" />
+ //       <p>
+ //        React Prueba.
+ //       </p>
+ //       <a
+  //        className="App-link"
+ //         href="https://reactjs.org"
+//          target="_blank"
+ //         rel="noopener noreferrer"
+ //       >
+ //         AAAA
+ //       </a>
+ //     </header>
+ //   </div>
+ // );
+//}
+
+//export default App;
